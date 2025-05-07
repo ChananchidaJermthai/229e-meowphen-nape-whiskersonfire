366ce5a6097534f6402312e22853a5f07d39091a
ChananchidaJermthai <wowchananchida@gmail.com>
2025-05-07 12:28:59 +0700

    Finish Credits Scene

---
+++ Assets/SCRIPTS/UI Scirpt/OpenLinkCredits.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class OpenLinkCrdits : MonoBehaviour
+{
+    public void OpenWebsite()
+    {
+        Application.OpenURL("https://github.com/ChananchidaJermthai/CreditsAsset/issues/1#issue-3044724626");
+    }
+}
